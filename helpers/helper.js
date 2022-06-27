a37ba5f78baef9811506fbc466f3163c8a718bfb
Manas Ranjan <manas.nilorout@uipath.com>
2022-06-27 13:41:01 +0530

    Added surprise me support!

--- helpers/helper.js
+++ helpers/helper.js
@@ -2,4 +2,5 @@
 
 module.exports = {
     getRandomInt: (max) => Math.floor(Math.random() * max),
+    getCapitalizedWord: (word) => word.charAt(0).toUpperCase() + word.slice(1),
 };
---
+++ slack/message-templates/connectedMessage.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    text: 'Connected you here!',
+    blocks: [
+        {
+            type: 'section',
+            fields: [
+                {
+                    type: 'mrkdwn',
+                    text: "*Hey guys!*\nI have connected you both here!\nFeel free to talk anything.\nIf you want a kick start let me know by typing `/ask_a_question` and I'll pop a question for you.\nYou can also start playing a online game, I can suggest you one when you type `/suggest_game`\nHappy chatting!"
+                }
+            ]
+        }
+    ]
+};
